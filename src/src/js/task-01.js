925da842cb98a9a8fae447e342187f2c039e1cf1
Natalia Kozak <90988795+Natalja21@users.noreply.github.com>
2022-04-16 17:25:52 +0300

    task-01

---
+++ src/src/js/task-01.js
@@ -0,0 +1,12 @@
+
+
+const categoriesRefs = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesRefs.length}`);
+
+const categoriesArray = [...categoriesRefs]
+    .map(
+        el => `Category: ${el.children[0].textContent}
+Elements: ${el.children[1].children.length}`
+    )
+    .join("\n");
+console.log(categoriesArray);
